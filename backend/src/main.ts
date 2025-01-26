f777e05183ea7f0d5162ed56cb0b37176223c810
RamonDonadeu <ramondonadeucaballero@gmail.com>
2025-01-26 10:54:52 +0100

    refactor: more changes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,5 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3001);
+  console.log(`Server started on port ${process.env.PORT}`);
 }
 bootstrap();
