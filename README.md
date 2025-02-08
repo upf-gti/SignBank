6c87f35a35b3787827d3a3239dea1bf6822ae68c
Turbobot <turbobot@vercel.com>
2025-02-08 11:09:45 +0100

    feat(create-turbo): apply official-starter transform

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ TODOS
 - Unit testing
 - Code coverage
 - Integration testing
+
+How to start project locally
+
+docker-compose -f docker-compose-local.yaml build
+docker-compose -f docker-compose-local.yaml up
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(3001);
   console.log(`Server started on port ${process.env.PORT}`);
 }
 bootstrap();
