430e71028a21086780be4f7ab07a481cb2de3661
RamonDonadeu <ramondonadeucaballero@gmail.com>
2025-02-23 12:06:22 +0100

    refactor: migrate to quasar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,7 @@
-<script setup lang="ts">
-import headerComponent from './components/headerComponent.vue'
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div class="body">
-    <headerComponent />
-    <RouterView />
-  </div>
+  <router-view />
 </template>
 
-<style scoped>
-@import url('css/app.css');
-
-.body {
-  display: flex;
-  flex-direction: column;
-  height: 100dvh;
-  width: 100dvw;
-}
-</style>
+<script setup lang="ts">
+//
+</script>
