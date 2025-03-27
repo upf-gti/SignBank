4f8ef581b3a7c792b58c7e7a70a8f613ceb10671
RamonDonadeu <ramondonadeucaballero@gmail.com>
2025-03-27 09:14:08 +0100

    refactor: allow connection from outside docker

--- mongoDB/initdb.d/init-replica.js
+++ mongoDB/initdb.d/init-replica.js
@@ -2,7 +2,8 @@
 rsconf = {
     _id: "rs0",
     members: [
-        { _id: 0, host: "mongoDB:27017" }
+        { _id: 0, host: "mongoDB" },
+        { _id: 1, host: "localhost" },
     ]
 }
 
@@ -22,7 +23,7 @@ admin.createUser({
 // Wait for the replica set to be initialized
 while (!rs.isMaster().ismaster) {
     print("Waiting for replica set to be initialized...")
-    sleep(1000)
+    sleep(10)
 }
 
 print("Replica set initialized successfully!") 
